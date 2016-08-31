1a38267a51a13c6d21c013f8bb04e282c36ef343
bogdanvlviv <bogdanvlviv@gmail.com>
2016-08-31 19:23:08 +0300

    Fix install redis
    Using `cd` in bash scripts isn't good idea.

--- ubuntu/redis/install-redis.sh
+++ ubuntu/redis/install-redis.sh
@@ -8,12 +8,10 @@ apt-get install -y curl
 apt-get install -y build-essential
 
 # http://redis.io/topics/quickstart
-cd /tmp
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-make install
+curl http://download.redis.io/redis-stable.tar.gz -o /tmp/redis-stable.tar.gz
+tar xvzf /tmp/redis-stable.tar.gz -C /tmp
+make -C /tmp/redis-stable
+make install -C /tmp/redis-stable
 mkdir /etc/redis
 mkdir /var/redis
 cp /tmp/redis-stable/utils/redis_init_script /etc/init.d/redis_6379
@@ -21,3 +19,5 @@ cp /tmp/redis-stable/redis.conf /etc/redis/6379.conf
 mkdir /var/redis/6379
 sed -i "s/dir \.\//dir \/var\/redis\/6379/g" /etc/redis/6379.conf
 sed -i "s/daemonize no/daemonize yes/g" /etc/redis/6379.conf
+rm /tmp/redis-stable.tar.gz
+rm -fr /tmp/redis-stable
