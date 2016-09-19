ccf7399801e0d753750239bdeae18c9a1684bffb
bogdanvlviv <bogdanvlviv@gmail.com>
2016-09-19 19:00:34 +0300

    Add GatewayPorts clientspecified

--- ubuntu/ssh/change-sshd_config.sh
+++ ubuntu/ssh/change-sshd_config.sh
@@ -5,4 +5,6 @@ sed -i "/Port/c\Port $1" /etc/ssh/sshd_config
 sed -i "/PasswordAuthentication yes/c\PasswordAuthentication $2" /etc/ssh/sshd_config
 sed -i "/PasswordAuthentication no/c\PasswordAuthentication $2" /etc/ssh/sshd_config
 
+sed -i "1iGatewayPorts clientspecified\n" /etc/ssh/sshd_config
+
 sudo /etc/init.d/ssh restart
