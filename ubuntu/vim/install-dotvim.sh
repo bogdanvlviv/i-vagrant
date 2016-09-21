5785568d675662f022b5ba5b2c13427568078810
bogdanvlviv <bogdanvlviv@gmail.com>
2016-09-21 12:10:10 +0300

    Add rm .vimrc and Xresources

--- ubuntu/vim/install-dotvim.sh
+++ ubuntu/vim/install-dotvim.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install -y git
 
 rm -fr ~/.vim
+rm ~/.vimrc
 git clone https://github.com/bogdanvlviv/dotvim.git ~/.vim
 cd ~/.vim
 git submodule update --init --recursive
--- ubuntu/xterm/install-dotX.sh
+++ ubuntu/xterm/install-dotX.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install -y git
 
 rm -fr ~/.X
+rm ~/.Xresources
 git clone https://github.com/bogdanvlviv/dotX.git ~/.X
 ln -s ~/.X/.Xresources ~/
 xrdb ~/.Xresources
