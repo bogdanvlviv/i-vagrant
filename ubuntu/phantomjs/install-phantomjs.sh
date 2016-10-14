65e5fdf9fc64e75ded7b80edc570f89f771076f7
bogdanvlviv <bogdanvlviv@gmail.com>
2016-10-14 11:47:06 +0300

    /bin -> /usr/local/bin

--- ubuntu/phantomjs/install-phantomjs.sh
+++ ubuntu/phantomjs/install-phantomjs.sh
@@ -6,6 +6,6 @@ apt-get install -y curl
 
 curl -L https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -o /tmp/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xvjf /tmp/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /tmp
-cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/phantomjs
+cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
 rm /tmp/phantomjs-2.1.1-linux-x86_64.tar.bz2
 rm -fr /tmp/phantomjs-2.1.1-linux-x86_64
