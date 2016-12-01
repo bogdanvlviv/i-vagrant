e07028f851c08bb11740567c6067593e468495e8
bogdanvlviv <bogdanvlviv@gmail.com>
2016-12-01 21:51:24 +0200

    Update install-redis.sh

--- ubuntu/redis/install-redis.sh
+++ ubuntu/redis/install-redis.sh
@@ -7,17 +7,15 @@ apt-get install -y curl
 
 apt-get install -y build-essential
 
-# http://redis.io/topics/quickstart
 curl http://download.redis.io/redis-stable.tar.gz -o /tmp/redis-stable.tar.gz
 tar xvzf /tmp/redis-stable.tar.gz -C /tmp
 make -C /tmp/redis-stable
 make install -C /tmp/redis-stable
 mkdir /etc/redis
-mkdir /var/redis
-cp /tmp/redis-stable/utils/redis_init_script /etc/init.d/redis_6379
+mkdir /var/lib/redis
+cp /tmp/redis-stable/utils/redis_init_script /etc/init.d/redis
 cp /tmp/redis-stable/redis.conf /etc/redis/6379.conf
-mkdir /var/redis/6379
-sed -i "s/dir \.\//dir \/var\/redis\/6379/g" /etc/redis/6379.conf
-sed -i "s/daemonize no/daemonize yes/g" /etc/redis/6379.conf
+sed -i "s/^dir \.\//dir \/var\/lib\/redis/g" /etc/redis/6379.conf
+sed -i "s/^daemonize no/daemonize yes/g" /etc/redis/6379.conf
 rm /tmp/redis-stable.tar.gz
 rm -fr /tmp/redis-stable
