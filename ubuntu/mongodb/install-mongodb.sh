725218eb291d5e2a1cb06f895acf3d161f022952
bogdanvlviv <bogdanvlviv@gmail.com>
2017-03-24 20:52:35 +0200

    Update install mongodb

--- ubuntu/mongodb/install-mongodb.sh
+++ ubuntu/mongodb/install-mongodb.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/env bash
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu $2/mongodb-org/$1 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-$1.list > /dev/null
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu $2/mongodb-org/$1 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-$1.list > /dev/null
 apt-get update
 
 apt-get install -y mongodb-org
-
-if (( $2 = "xenial" )); then
-  echo "[Unit]" | tee /lib/systemd/system/mongod.service > /dev/null
-  echo "Description=High-performance, schema-free document-oriented database" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "After=network.target" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "Documentation=https://docs.mongodb.org/manual" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "[Service]" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "User=mongodb" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "Group=mongodb" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "[Install]" | tee -a /lib/systemd/system/mongod.service > /dev/null
-  echo "WantedBy=multi-user.target" | tee -a /lib/systemd/system/mongod.service > /dev/null
-fi
 
 shift 2
 
