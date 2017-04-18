47e95c25b5816c1fc58167193049d2a2e19c37ca
bogdanvlviv <bogdanvlviv@gmail.com>
2017-04-18 21:10:02 +0300

    Update fzf default command

--- ubuntu/fzf/set-fzf_default_command.sh
+++ ubuntu/fzf/set-fzf_default_command.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -i "1iexport FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir .git -g \"\"'\n" ~/.bashrc
+sed -i "1iexport FZF_DEFAULT_COMMAND='ag --unrestricted -g \"\"'\n" ~/.bashrc
