0584d81062cb62ec96ee2b201d6baa4674fe75a8
bogdanvlviv <bogdanvlviv@gmail.com>
2017-07-02 13:13:59 +0300

    Install latest tmux

--- ubuntu/tmux/install-tmux.sh
+++ ubuntu/tmux/install-tmux.sh
@@ -4,4 +4,18 @@ if [[ "$RUN_APT_GET_UPDATE_BEFORE" != "no" ]]; then
   apt-get update
 fi
 
-apt-get install -y tmux
+# "ubuntu/git/install-git.sh"
+apt-get install -y git
+# "ubuntu/git/install-git.sh"
+
+apt-get install -y build-essential libevent-dev
+
+rm -fr /tmp/tmux
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+cd /tmp/tmux
+sh autogen.sh
+./configure && make
+make install
+cd -
+
+rm -fr /tmp/tmux
