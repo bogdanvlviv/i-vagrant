2dc2e72e11a1722edfc823c73316637505949c41
bogdanvlviv <bogdanvlviv@gmail.com>
2017-07-22 23:05:53 +0300

    Add install automake during install tmux

--- ubuntu/tmux/install-tmux.sh
+++ ubuntu/tmux/install-tmux.sh
@@ -11,6 +11,7 @@ apt-get install -y git
 apt-get install -y build-essential
 apt-get install -y libevent-dev
 apt-get install -y pkg-config
+apt-get install -y automake
 
 rm -fr /tmp/tmux
 git clone https://github.com/tmux/tmux.git /tmp/tmux
