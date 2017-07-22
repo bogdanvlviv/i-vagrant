4291f23cbba7afadb94d8a08d992a594623ce705
bogdanvlviv <bogdanvlviv@gmail.com>
2017-07-22 13:29:21 +0300

    Note down about problems with permissions for ~/.rbenv/

--- ubuntu/ruby/install-rbenv.sh
+++ ubuntu/ruby/install-rbenv.sh
@@ -17,3 +17,6 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 ~/.rbenv/bin/rbenv init
 sed -i "1ieval \"\$(rbenv init -)\"\n" ~/.bashrc
 sed -i "1iexport PATH=\"\$HOME/.rbenv/bin:\$PATH\"" ~/.bashrc
+
+# If you have problems with permissions, run
+# sudo chown -R username:username ~/.rbenv/
