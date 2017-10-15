a4e70d8acca4c69468c4a2215c5d3a2bbc24cb3a
bogdanvlviv <bogdanvlviv@gmail.com>
2017-10-15 15:46:40 +0300

    Add USER_NAME option for rbenv

--- ubuntu/ruby/install-rbenv.sh
+++ ubuntu/ruby/install-rbenv.sh
@@ -18,5 +18,4 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 sed -i "1ieval \"\$(rbenv init -)\"\n" ~/.bashrc
 sed -i "1iexport PATH=\"\$HOME/.rbenv/bin:\$PATH\"" ~/.bashrc
 
-# If you have problems with permissions, run
-# sudo chown -R username:username ~/.rbenv/
+sudo chown -R $1:$1 ~/.rbenv/
