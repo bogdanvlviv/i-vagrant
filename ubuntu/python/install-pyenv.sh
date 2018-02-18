5ca94ae9af00e4aeaebae7e8279e3c49f46c29a2
bogdanvlviv <bogdanvlviv@gmail.com>
2018-02-18 20:59:02 +0200

    Add chown -R :

--- ubuntu/python/install-pyenv.sh
+++ ubuntu/python/install-pyenv.sh
@@ -15,5 +15,6 @@ sed -i "1ieval \"\$(pyenv init -)\"\n" ~/.bashrc
 sed -i "1iexport PATH=\"\$PYENV_ROOT/bin:\$PATH\"" ~/.bashrc
 sed -i "1iexport PYENV_ROOT=\"\$HOME/.pyenv\"" ~/.bashrc
 
-# If you have problems with permissions, run
-# sudo chown -R username:username ~/.pyenv/
+if [[ "$SUDO_USER" ]]; then
+  chown -R $SUDO_USER:$SUDO_USER ~/.pyenv/
+fi
--- ubuntu/ruby/install-rbenv.sh
+++ ubuntu/ruby/install-rbenv.sh
@@ -18,5 +18,6 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 sed -i "1ieval \"\$(rbenv init -)\"\n" ~/.bashrc
 sed -i "1iexport PATH=\"\$HOME/.rbenv/bin:\$PATH\"" ~/.bashrc
 
-# If you have problems with permissions, run
-# sudo chown -R username:username ~/.rbenv/
+if [[ "$SUDO_USER" ]]; then
+  chown -R $SUDO_USER:$SUDO_USER ~/.rbenv/
+fi
