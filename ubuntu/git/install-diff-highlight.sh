7a85068fa1c0154574b406a5c9bd70bc3dbfebb8
bogdanvlviv <bogdanvlviv@gmail.com>
2018-11-25 23:40:02 +0200

    Ensure that diff-highlight is executable

--- ubuntu/git/install-diff-highlight.sh
+++ ubuntu/git/install-diff-highlight.sh
@@ -3,3 +3,4 @@
 make -C /usr/share/doc/git/contrib/diff-highlight
 
 cp /usr/share/doc/git/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+chmod 755 /usr/local/bin/diff-highlight
