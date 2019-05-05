ca024701501ee53ba5be1330945a0a76de50e9bd
bogdanvlviv <bogdanvlviv@gmail.com>
2019-05-05 15:15:26 +0300

    Fix ruby-build
    ```
    $ sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev li
    bncurses5-dev libffi-dev libgdbm3 libgdbm-dev
    Reading package lists... Done
    Building dependency tree
    Reading state information... Done
    Note, selecting 'libreadline-dev' instead of 'libreadline6-dev'
    E: Unable to locate package libgdbm3
    ```

--- ubuntu/ruby/install-ruby-build.sh
+++ ubuntu/ruby/install-ruby-build.sh
@@ -10,7 +10,17 @@ apt install -y git
 apt install -y curl
 # "ubuntu/curl/install-curl.sh"
 
-apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+apt install -y autoconf
+apt install -y bison
+apt install -y build-essential
+apt install -y libssl-dev
+apt install -y libyaml-dev
+apt install -y libreadline6-dev
+apt install -y zlib1g-dev
+apt install -y libncurses5-dev
+apt install -y libffi-dev
+apt install -y libgdbm3
+apt install -y libgdbm-dev
 
 rm -fr ~/.rbenv/plugins/ruby-build
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
