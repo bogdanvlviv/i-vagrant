35cfe594624d3df49b3b7aa85efbc67779bdcbac
bogdanvlviv <bogdanvlviv@gmail.com>
2019-05-10 23:51:56 +0300

    chmod chromedriver

--- ubuntu/google-chrome/install-chromedriver.sh
+++ ubuntu/google-chrome/install-chromedriver.sh
@@ -13,5 +13,6 @@ rm -fr /tmp/chromedriver_linux64
 curl -L https://chromedriver.storage.googleapis.com/$1/chromedriver_linux64.zip -o /tmp/chromedriver_linux64.zip
 unzip /tmp/chromedriver_linux64.zip -d /tmp/chromedriver_linux64
 cp /tmp/chromedriver_linux64/chromedriver /usr/local/bin/chromedriver
+chmod 755 /usr/local/bin/chromedriver
 rm /tmp/chromedriver_linux64.zip
 rm -fr /tmp/chromedriver_linux64
