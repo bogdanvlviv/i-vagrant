bc2caf0e42cc6a6b35dfe067c0cae39f796d9e33
bogdanvlviv <bogdanvlviv@gmail.com>
2021-05-03 22:57:02 +0300

    Fix broken dependencies of wkhtmltopdf

--- ubuntu/wkhtmltopdf/install-wkhtmltopdf.sh
+++ ubuntu/wkhtmltopdf/install-wkhtmltopdf.sh
@@ -6,11 +6,11 @@ apt update
 apt install -y curl
 # "ubuntu/curl/install-curl.sh"
 
-apt install -y xfonts-75dpi
-
 rm /tmp/wkhtmltopdf-$1.deb
 curl -L https://github.com/wkhtmltopdf/packaging/releases/download/$1-1/wkhtmltox_$1-1.$2_amd64.deb -o /tmp/wkhtmltopdf-$1.deb
 
 dpkg -i /tmp/wkhtmltopdf-$1.deb
 
 rm /tmp/wkhtmltopdf-$1.deb
+
+apt --fix-broken install -y
