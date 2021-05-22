61d03ee07e8c13c16a0afe794b2d838d3b561740
bogdanvlviv <bogdanvlviv@gmail.com>
2021-05-22 22:47:54 +0300

    Move back to github

--- ubuntu/tmux/install-dottmux.sh
+++ ubuntu/tmux/install-dottmux.sh
@@ -12,9 +12,8 @@ fi
 
 rm -fr ~/.tmux
 rm ~/.tmux.conf
-git clone https://gitlab.com/bogdanvlviv/dottmux.git ~/.tmux
+git clone https://github.com/bogdanvlviv/dottmux.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/
 cd ~/.tmux
 git remote add github git@github.com:bogdanvlviv/dottmux.git
-git remote add gitlab git@gitlab.com:bogdanvlviv/dottmux.git
 cd -
--- ubuntu/xterm/install-dotX.sh
+++ ubuntu/xterm/install-dotX.sh
@@ -12,10 +12,9 @@ fi
 
 rm -fr ~/.X
 rm ~/.Xresources
-git clone https://gitlab.com/bogdanvlviv/dotX.git ~/.X
+git clone https://github.com/bogdanvlviv/dotX.git ~/.X
 ln -s ~/.X/.Xresources ~/
 xrdb ~/.Xresources
 cd ~/.X
 git remote add github git@github.com:bogdanvlviv/dotX.git
-git remote add gitlab git@gitlab.com:bogdanvlviv/dotX.git
 cd -
