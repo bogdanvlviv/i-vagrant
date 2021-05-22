067f8807d8bef517a1a365e299f324578c6038c9
bogdanvlviv <bogdanvlviv@gmail.com>
2021-05-22 23:35:52 +0300

    Revert "Rename dotvim branch to main"
    This reverts commit 04009d936f912196c7c07156f15995ff6f980a81.

--- ubuntu/vim/install-dotvim.sh
+++ ubuntu/vim/install-dotvim.sh
@@ -15,7 +15,7 @@ rm ~/.vimrc
 git clone https://github.com/bogdanvlviv/dotvim.git ~/.vim
 cd ~/.vim
 git submodule update --init --recursive
-git submodule foreach --recursive 'git pull origin main'
+git submodule foreach --recursive 'git pull origin master'
 ln -s bundle/vim-pathogen/autoload/ ./
 git remote add github git@github.com:bogdanvlviv/dotvim.git
 cd -
