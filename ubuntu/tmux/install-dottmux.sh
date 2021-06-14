04917e8f5ab92e9d0ce80643d57878fda89fa700
bogdanvlviv <bogdanvlviv@gmail.com>
2021-06-14 22:58:59 +0300

    Revert "Move back to github"
    This reverts commit 61d03ee07e8c13c16a0afe794b2d838d3b561740.

--- ubuntu/tmux/install-dottmux.sh
+++ ubuntu/tmux/install-dottmux.sh
@@ -12,10 +12,11 @@ fi
 
 rm -fr ~/.tmux
 rm ~/.tmux.conf
-git clone https://github.com/bogdanvlviv/dottmux.git ~/.tmux
+git clone https://gitlab.com/bogdanvlviv/dottmux.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/
 cd ~/.tmux
 git remote add github git@github.com:bogdanvlviv/dottmux.git
+git remote add gitlab git@gitlab.com:bogdanvlviv/dottmux.git
 cd -
 
 if [[ "$SUDO_USER" ]]; then
--- ubuntu/vim/install-dotvim.sh
+++ ubuntu/vim/install-dotvim.sh
@@ -12,12 +12,13 @@ fi
 
 rm -fr ~/.vim
 rm ~/.vimrc
-git clone https://github.com/bogdanvlviv/dotvim.git ~/.vim
+git clone https://gitlab.com/bogdanvlviv/dotvim.git ~/.vim
 cd ~/.vim
 git submodule update --init --recursive
 git submodule foreach --recursive 'git pull origin master'
 ln -s bundle/vim-pathogen/autoload/ ./
 git remote add github git@github.com:bogdanvlviv/dotvim.git
+git remote add gitlab git@gitlab.com:bogdanvlviv/dotvim.git
 cd -
 
 if [[ "$SUDO_USER" ]]; then
