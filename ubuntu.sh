a620d7677d265bec75c812b6bd080590ec90378e
bogdanvlviv <bogdanvlviv@gmail.com>
2021-09-29 20:51:02 +0300

    Migrate ruby from rbenv to asdf

--- ubuntu.sh
+++ ubuntu.sh
@@ -194,13 +194,7 @@ bash "ubuntu/readline/set-inputrc.sh"
 # bash "ubuntu/redis/asdf-install-redis.sh" 6.2.5
 
 # ruby
-sudo bash "ubuntu/ruby/install-rbenv.sh"
-sudo bash "ubuntu/ruby/install-ruby-build.sh"
-sudo bash "ubuntu/ruby/install-rbenv-ctags.sh"
-sudo bash "ubuntu/ruby/install-rbenv-vars.sh"
-sudo bash "ubuntu/ruby/install-ruby.sh" rbenv 3.0.2 break rubocop "tmuxinator:3.0.1" debug pry-byebug
-#
-# bash "ubuntu/ruby/asdf-install-ruby.sh" 3.0.2 break rubocop "tmuxinator:3.0.1" debug pry-byebug
+bash "ubuntu/ruby/asdf-install-ruby.sh" 3.0.2 break rubocop "tmuxinator:3.0.1" debug pry-byebug
 # bash "ubuntu/ruby/prepare-for-rails-development.sh" $USER root admin
 
 # sequeler
--- ubuntu/ruby/asdf-install-ruby.sh
+++ ubuntu/ruby/asdf-install-ruby.sh
@@ -11,7 +11,7 @@ sudo apt install -y libreadline-dev
 sudo apt install -y zlib1g-dev
 sudo apt install -y libncurses5-dev
 sudo apt install -y libffi-dev
-sudo apt install -y libgdbm3
+sudo apt install -y libgdbm6
 sudo apt install -y libgdbm-dev
 
 if [[ "$SUDO_USER" ]]; then
