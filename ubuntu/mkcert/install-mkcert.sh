2cc86d2dc3526d0a8cdd75c7f5b5055c82983622
bogdanvlviv <bogdanvlviv@gmail.com>
2021-10-21 11:28:37 +0300

    update mkcert

--- ubuntu/mkcert/install-mkcert.sh
+++ ubuntu/mkcert/install-mkcert.sh
@@ -2,11 +2,11 @@
 
 apt update
 
-apt install -y libnss3-tools
-
 # "ubuntu/curl/install-curl.sh"
 apt install -y curl
 # "ubuntu/curl/install-curl.sh"
+
+apt install -y libnss3-tools
 
 curl -L https://github.com/FiloSottile/mkcert/releases/download/$1/mkcert-$1-linux-amd64 -o /usr/local/bin/mkcert
 chmod 755 /usr/local/bin/mkcert
