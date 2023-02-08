59210d007a4482b6e847ddb69efbbbeb360fad20
bogdanvlviv <bogdanvlviv@gmail.com>
2023-02-08 15:52:59 +0200

    update chromedriver to 110

--- ubuntu.sh
+++ ubuntu.sh
@@ -78,7 +78,7 @@ source "ubuntu/go/asdf-install-go.sh" 1.19
 
 # google-chrome
 source "ubuntu/google-chrome/install-google-chrome.sh"
-source "ubuntu/google-chrome/install-chromedriver.sh" 109.0.5414.74
+source "ubuntu/google-chrome/install-chromedriver.sh" 110.0.5481.77
 
 # graphiql
 # source "ubuntu/graphiql/install-graphiql.sh" 0.7.2
