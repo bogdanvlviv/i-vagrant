02188de3b8f07c3424731899ff744b387e56e87b
bogdanvlviv <bogdanvlviv@gmail.com>
2023-06-02 15:19:22 +0300

    update chromedriver to 114

--- ubuntu.sh
+++ ubuntu.sh
@@ -78,7 +78,7 @@ source "ubuntu/go/asdf-install-go.sh" 1.19
 
 # google-chrome
 source "ubuntu/google-chrome/install-google-chrome.sh"
-source "ubuntu/google-chrome/install-chromedriver.sh" 113.0.5672.63
+source "ubuntu/google-chrome/install-chromedriver.sh" 114.0.5735.90
 
 # graphiql
 # source "ubuntu/graphiql/install-graphiql.sh" 0.7.2
