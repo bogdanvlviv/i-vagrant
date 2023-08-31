7858e07af6e9574532ae75a4ed9378a1ce44ef53
bogdanvlviv <bogdanvlviv@gmail.com>
2023-08-31 17:16:01 +0300

    install chromedriver 116

--- ubuntu.sh
+++ ubuntu.sh
@@ -78,7 +78,7 @@ source "ubuntu/go/asdf-install-go.sh" 1.19
 
 # google-chrome
 source "ubuntu/google-chrome/install-google-chrome.sh"
-source "ubuntu/google-chrome/install-chromedriver.sh" 114.0.5735.90
+source "ubuntu/google-chrome/install-chromedriver.sh" 116.0.5845.96
 
 # graphiql
 # source "ubuntu/graphiql/install-graphiql.sh" 0.7.2
--- ubuntu/google-chrome/install-chromedriver.sh
+++ ubuntu/google-chrome/install-chromedriver.sh
@@ -8,9 +8,9 @@ sudo apt install -y unzip
 
 rm /tmp/chromedriver_linux64.zip
 rm -fr /tmp/chromedriver_linux64
-curl -L https://chromedriver.storage.googleapis.com/$1/chromedriver_linux64.zip -o /tmp/chromedriver_linux64.zip
+curl -L https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$1/linux64/chromedriver-linux64.zip -o /tmp/chromedriver_linux64.zip
 unzip /tmp/chromedriver_linux64.zip -d /tmp/chromedriver_linux64
-sudo cp /tmp/chromedriver_linux64/chromedriver /usr/local/bin/chromedriver
+sudo cp /tmp/chromedriver_linux64/chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
 sudo chmod 755 /usr/local/bin/chromedriver
 rm /tmp/chromedriver_linux64.zip
 rm -fr /tmp/chromedriver_linux64
