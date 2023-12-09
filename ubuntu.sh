3886ccc665017ed45d0f994ee980561104050a93
bogdanvlviv <bogdanvlviv@gmail.com>
2023-12-09 21:29:19 +0200

    Update zoom to 5.16.10.668

--- ubuntu.sh
+++ ubuntu.sh
@@ -274,7 +274,7 @@ source "ubuntu/xterm/install-dotX.sh"
 source "ubuntu/yubikey-manager/install-yubikey-manager.sh"
 
 # zoom
-source "ubuntu/zoom/install-zoom.sh" 5.13.5.431
+source "ubuntu/zoom/install-zoom.sh" 5.16.10.668
 
 echo ""
 echo "Running: \`source \"$HOME/.bashrc\"\`"
