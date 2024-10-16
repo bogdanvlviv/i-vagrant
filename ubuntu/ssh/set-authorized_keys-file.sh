9bae8318e0fe15ffc18e7dc27769f947420783a0
Bogdan <bogdanvlviv@gmail.com>
2024-10-16 11:19:44 +0300

    Update my ssh key to ssh-ed25519

--- ubuntu/ssh/set-authorized_keys-file.sh
+++ ubuntu/ssh/set-authorized_keys-file.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
 cat <<EOF | tee ~/.ssh/authorized_keys > /dev/null
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjfsK7Uvp8ZtGcb+PDPfzwGvjZYr7PKYo3vrUTsjZkqCXP6sLgsddQbRFEWls9ikuYftquYVmHQ8XYAmJMNNdhKnYdOPZpqkgUn2XViwE4vlNidsnf+X/snz/EhbFLe6tliCurO4IOomxh+vKfcaH7xMwFiX76mXCVLTcwu/d+3oGuPdGJ/+CmZVffHQn6iiyDpGlt43OkyFKPUcFRPXXixkAKXCZTJRelorIyBn87dLgnxFVFhX5eqIWfAdJl/r2Pm+R54aYJnBOkSKOn9ANAVS9BLJgv50RpRyGBemTFWLWZ3BeUXKf34j2hRtaVSA08lQDSzkl4HBsHUL0/xPdd bogdanvlviv@gmail.com
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIlu1vxetVEKpT9l1eC9DAkdSRUK/XyTPWxMPTv5lFLn bogdanvlviv@gmail.com
 EOF
 
 chmod 600 ~/.ssh/authorized_keys
