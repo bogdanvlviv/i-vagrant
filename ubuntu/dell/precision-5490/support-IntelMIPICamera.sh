406088f05c21b504b9d1c25255deb4c32ca480dc
Bogdan <bogdanvlviv@gmail.com>
2026-02-12 23:52:12 +0200

    Update instructions about support IntelMIPICamera

--- ubuntu/dell/precision-5490/support-IntelMIPICamera.sh
+++
@@ -1,5 +0,0 @@
-# https://wiki.ubuntu.com/IntelMIPICamera
-
-sudo apt update
-
-sudo apt-get install --no-install-recommends --yes linux-generic-hwe-24.04 linux-modules-ipu6-generic-hwe-24.04 linux-modules-usbio-generic-hwe-24.04
